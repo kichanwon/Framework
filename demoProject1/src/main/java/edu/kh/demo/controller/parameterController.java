2b8227abd42c6d33b562aed79d987a5d9d34d33a
kichanwon <65401632+kichanwon@users.noreply.github.com>
2025-04-29 14:15:19 +0900

    comiiiiiit all

--- demoProject1/src/main/java/edu/kh/demo/controller/parameterController.java
+++ demoProject1/src/main/java/edu/kh/demo/controller/parameterController.java
@@ -1,12 +1,19 @@
 package edu.kh.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.kh.demo.model.dto.MemberDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -34,5 +41,48 @@ public class parameterController {
 		log.debug("inputAddress	:" + inputAddress);
 
 		return "redirect:main";
+//		return "redirect:/param/main";
 	}
+	
+	@PostMapping("test2")
+	public String paramTest2(
+				@RequestParam("bookTitle") String bookTitle,
+				@RequestParam("bookAuthor") String bookAuthor,
+				@RequestParam(value="bookPrice", required=false, defaultValue="0") int bookPrice,
+				@RequestParam("bookPublisher") String bookPublisher
+//				@RequestParam(value="key"[, required=false, defaultValue="1"]) Type name
+				){
+		
+		System.out.println("/param/test2 post");
+		
+		log.debug("bookTitle : " + bookTitle);
+		log.debug("bookAuthor : " + bookAuthor);
+		log.debug("bookPrice : " + bookPrice);
+		log.debug("bookPublisher : " + bookPublisher);
+
+		return "redirect:main";
+	}
+	
+	@PostMapping("test3")
+	public String paramTest3(
+				@RequestParam("color") String[] colorArr,
+				@RequestParam("fruit") List<String> fruitList,
+				@RequestParam Map<String, Object> paramMap
+				){
+				
+		log.debug("colorArr : " + Arrays.toString(colorArr));
+		log.debug("fruitList : " + fruitList);
+		log.debug("paramMap : " + paramMap);
+
+		return "redirect:main";
+	}
+	
+	@PostMapping("test4")
+	public String paramTest4(@ModelAttribute MemberDTO inputMember){
+				
+		log.debug("inputMember : " + inputMember);
+
+		return "redirect:main";
+	}
+	
 }
